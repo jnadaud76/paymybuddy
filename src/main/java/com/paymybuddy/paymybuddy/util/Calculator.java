397d3d6b8181f61d63422365dfcd7f4852ead2e8
Johan Nadaud <johan.nadaud.pro@gmail.com>
2022-04-21 12:28:04 +0200

    Add invoicing functionality.

--- src/main/java/com/paymybuddy/paymybuddy/util/Calculator.java
+++ src/main/java/com/paymybuddy/paymybuddy/util/Calculator.java
@@ -4,6 +4,7 @@ import com.paymybuddy.paymybuddy.model.Person;
 import com.paymybuddy.paymybuddy.repository.PersonRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -12,11 +13,14 @@ public class Calculator {
     @Autowired
     PersonRepository personRepository;
 
+    @Value("${feeRate}")
+    private double feeRate;
+
     public void updateAmountAvailable(Integer recipientId, Integer senderId, Integer amount) {
         Person recipient = personRepository.findById(recipientId).get();
         Person sender = personRepository.findById(senderId).get();
-        if (sender.getAmountAvailable() >= amount) {
-            sender.setAmountAvailable(sender.getAmountAvailable() - amount);
+        if (sender.getAmountAvailable() >= (amount+(amount * feeRate))) {
+            sender.setAmountAvailable(sender.getAmountAvailable() - (amount + (amount * feeRate)));
             recipient.setAmountAvailable(recipient.getAmountAvailable() + amount);
         } else {
             throw new IllegalArgumentException();
